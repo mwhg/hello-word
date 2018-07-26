03b4d3adb5791261bd272ff27747a7fa406b0605
Oliver Xi <oliverx@synnex.com>
2018-07-26 17:51:44 +0800

    add resource

--- org.kimrong.dict.ui/src/org/kimrong/dict/Resources.java
+++ org.kimrong.dict.ui/src/org/kimrong/dict/Resources.java
@@ -2,6 +2,6 @@ package org.kimrong.dict;
 
 public interface Resources {
 
-    String ROOT = "D:\\workshop\\synnex_2\\org.kimrong.dict.ui\\resource\\";
+    String ROOT = "D:\\__github\\hello-word\\org.kimrong.dict.ui\\resource\\";
 
 }
